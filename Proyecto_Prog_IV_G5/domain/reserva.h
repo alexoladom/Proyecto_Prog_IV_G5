3127570c0d4bb0134c626f82eb55dc8a442af2a4
Alex Olazabal Dominguez <alex.o@opendeusto.es>
2024-04-03 09:24:55 +0200

    Arreglada la reserva.c

--- Proyecto_Prog_IV_G5/domain/reserva.h
+++ Proyecto_Prog_IV_G5/domain/reserva.h
@@ -9,14 +9,14 @@
 typedef struct {
 
 	int id;
-	char fecha[11];
+	char * fecha;
 	int dniCliente;
 	int numeroHabitacion;
 	int numeroPlazaParking;
 
 }Reserva;
 
-Reserva crearReserva(int id, char fecha[11], int dniCliente, int numeroHabitacion, int numeroPlazaParking);
+Reserva crearReserva(int id, char *fecha, int dniCliente, int numeroHabitacion, int numeroPlazaParking);
 void imprimirReserva(Reserva r);
 
 
