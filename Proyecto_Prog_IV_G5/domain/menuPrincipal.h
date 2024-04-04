91b9b54207fef7e8ecce488fc3dbdacdb3cfffa1
Jorge Martinez <jorge.m@opendeusto.es>
2024-04-04 09:15:29 +0200

    Nuevos menus e implementaciones

--- Proyecto_Prog_IV_G5/domain/menuPrincipal.h
+++ Proyecto_Prog_IV_G5/domain/menuPrincipal.h
@@ -11,7 +11,27 @@ void registrarseCliente();
 void iniciarSesionAdmin();
 
 void menuTrabajo();
+
+void listadoClientes();
+void menuHabitaciones();
+void menuPlazaParking();
+
 void menuCompra();
+
+void reservarHabitacion();
+void reservarParking();
+void verEstadoReserva();
+void modificarReserva();
+void cancelarReserva();
+
+void verHabitacionesReservadas();
+void verHabitacionesDisponibles();
+void verHabitacionesOcupadas();
+void verEstadoLimpiezaHabitaciones();
+void menuEditarInformacionHabitaciones();
+
+void verPlazasParking();
+void editarInformacionPlazasParking();
 
 #endif
 
