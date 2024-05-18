9ebd9908514808877c277e464544cd117cde52c0
benat <benat@KonponPc>
2024-05-18 16:59:16 +0200

    funciones para leer el fichero

--- C++_Proyecyo_Prog_IV_G5/main.cpp
+++ C++_Proyecyo_Prog_IV_G5/main.cpp
@@ -1,4 +1,3 @@
-
 #include "db/funcionesBD.h"
 #include "sockets/funcionesSockets.h"
 #include "domain/cliente.h"
@@ -7,8 +6,58 @@
 #include "domain/reserva.h"
 #include "config/logger.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define OK 1
 #define NOT_OK 0
+
+typedef struct {
+    char ip[16];
+    int port;
+    char db_address[256];
+} Config;
+
+Config readConfig(const char* filename) {
+    Config config;
+    FILE* file = fopen(filename, "r");
+
+    if (!file) {
+        fprintf(stderr, "No se puede cargar el archivo de configuración %s\n", filename);
+        exit(1);
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        char* equals = strchr(line, '=');
+        if (equals) {
+            *equals = '\0';
+            char* key = line;
+            char* value = equals + 1;
+
+            // Quitar espacios en blanco alrededor de key y value
+            while (isspace((unsigned char)*key)) key++;
+            while (isspace((unsigned char)*value)) value++;
+            char* end = key + strlen(key) - 1;
+            while (end > key && isspace((unsigned char)*end)) end--;
+            end[1] = '\0';
+            end = value + strlen(value) - 1;
+            while (end > value && isspace((unsigned char)*end)) end--;
+            end[1] = '\0';
+
+            if (strcmp(key, "ip") == 0) {
+                strncpy(config.ip, value, sizeof(config.ip) - 1);
+            } else if (strcmp(key, "port") == 0) {
+                config.port = atoi(value);
+            } else if (strcmp(key, "db_address") == 0) {
+                strncpy(config.db_address, value, sizeof(config.db_address) - 1);
+            }
+        }
+    }
+    fclose(file);
+    return config;
+}
 
 int main(int argc, char **argv) {
 
@@ -201,4 +250,3 @@ int main(int argc, char **argv) {
 
 
 }
-
